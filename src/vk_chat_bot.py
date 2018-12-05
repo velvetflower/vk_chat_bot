9309ae7672286ab08ad138348c544b47ba1487d7
velvetflower <kotik.ug@mail.ru>
2018-12-05 23:41:26 +0300

    added sendMessage

--- src/vk_chat_bot.py
+++ src/vk_chat_bot.py
@@ -3,24 +3,41 @@ from time import gmtime, strftime
 
 token = ""
 api = vk_requests.create_api(service_token=token, scope=['friends', 'messages', 'wall', 'offline'])
+chatid = 1 #place here your conversation id
 
+def sendMessage(par):
+    print('# sendMessage called')
+    if par[0] == 1: #date
+        letsSendIt = api.messages.send(user_id=chatid, random_id=random.getrandbits(128), message=par[1])
+        print ('# sent!')
+        
 def searchMessage():
-    getMessage = api.messages.search(q='!bot',count=1)
+    print('# searchMessage called')
+    getMessage = api.messages.search(q='!бот',peer_id='124057393',count=1)
     getMessageBody = getMessage['items'][0]['body']
     return getMessageBody
 
 def initalizeRequest():
+    print('# initalizeRequest called')
     callMessage = searchMessage()
-    if callMessage == '!bot time':
-        _retTime()
-    elif callMessage == '!bot weather':
-        _retWearher()
+    print('# continue')
+    if callMessage == '!бот время':
+        print('# date callMessage found')
+        sendMessage(_retTime())
+    elif callMessage == '!бот погода':
+        print('# weather callMessage found')
+        return 0 # функция времени
     else:
+        print('# no messages found')
         return 0
 
 def _retTime():
-    return strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    print('# _reqTime called')
+    return (1, strftime("%Y-%m-%d %H:%M:%S", gmtime()))
 
-def _retWearher():
-    return 0 #todo
+while True:
+    print('# iteration')
+    initalizeRequest()
+    time.sleep(1)
+
     
